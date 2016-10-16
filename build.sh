9e8c54ea23c9dd4194e011b06e9739b60123d49c
David Schkalee <david.schkalee@sysvision.de>
2016-10-17 01:19:25 +0200

    Modified index.ejs in build process

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 cat node_modules/jquery/dist/jquery.js node_modules/underscore/underscore.js app/js/embed.js > public/embed.js
 cp -R app/tpl public/
 cp app/css/embed.css public/embed.css
+cp app/index.ejs public/index.ejs
